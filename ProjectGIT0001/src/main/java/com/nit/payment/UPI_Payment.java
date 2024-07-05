481955d9c3259ffe8a0099640f0bf2c84440f1fa
Vedavyas5007 <157232858+Vedavyas5007@users.noreply.github.com>
2024-07-05 13:37:44 -0400

    New payment package is created and UPI_Payment class is created and doPayment method is created

---
+++ ProjectGIT0001/src/main/java/com/nit/payment/UPI_Payment.java
@@ -0,0 +1,10 @@
+package com.nit.payment;
+
+public class UPI_Payment {
+	
+	public String doPayment(long phoneNo,double amount) {
+		System.out.println("Payment sucessfull");
+		return "Payment sucessfull";
+	}
+
+}
